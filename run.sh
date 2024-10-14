98b4aa292ce19a306519f3a8b6709179db04e588
Filip Kováč <filip.kovac@everifin.com>
2024-10-14 20:33:06 +0200

    add script to clean start app

---
+++ run.sh
@@ -0,0 +1,6 @@
+
+docker network create keywords-net 2>/dev/null
+
+docker compose build keywords
+docker compose up keywords --abort-on-container-exit --exit-code-from keywords 
+docker compose down
